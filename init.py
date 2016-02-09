4301107b3c6f9abe655bf7ceed8f310df425e775
Mateusz Pacek <pacolskb@gmail.com>
2016-02-09 22:38:45 +0000

    dodanie perc

--- init.py
+++ init.py
@@ -27,7 +27,7 @@ def cons_dict(q):
     cursor.execute(q)
     dict = {}
     for i in cursor:
-        dict[i[0]]=i[1]	
+        dict[i[0]]=i[1]
 
     cursor.close()
     cnx.close()
@@ -41,9 +41,6 @@ def top_visited(top_num):
             % top_num
             )
     return cons_dict(query)
-
-print top_visited(30)
-
 
 def top_stat(top_num):
     query = ("""select enu_http_status, count(*) as count 
@@ -59,18 +56,34 @@ def top_stat_by_ip(top_num):
             )
     return cons_dict(query)
 
+def top_bytes_per_site(top_num):
+    query = (""" select if (CHAR_LENGTH(var_request) > 20, 
+            CONCAT(LEFT(var_request, 20), '...'), var_request), 
+            sum(int_bytes) as sum from apache_log2  group by var_request 
+            order by sum desc limit %s;""" % top_num
+            )
+    return cons_dict(query)
+
+def perc_bytes_per_ip(top_num):
+    query = (""" select var_host, sum(int_bytes) / (select sum(int_bytes) 
+                from apache_log2) * 100 as t from apache_log2 group by var_host 
+                order by t desc limit %s""" % top_num
+                )
+    return cons_dict(query)
+
 # Generowanie wykresu
-def gen_chart(dict, output, chart):
+def gen_chart(dict, title, output, chart):
 
     if chart == "pie":
-        bar_chart = pygal.Pie()
+        _chart = pygal.Pie()
     elif chart == "bar":
-        bar_chart = pygal.Bar()
+        _chart = pygal.Bar()
     for i in dict.iteritems():
-        bar_chart.add(i[0], i[1])
+        _chart.add(i[0], i[1])
+    _chart.title=title
 
     # Sciezka do zapisu pliku
-    bar_chart.render_to_file('/var/www/%s' % output)
+    _chart.render_to_file('/var/www/%s' % output)
 
 
 
@@ -97,4 +110,4 @@ if __name__ == "__main__":
         usage()
         sys.exit(2)
 
-    gen_chart(top_visited(top_num), output, chart)
+    gen_chart(perc_bytes_per_ip(top_num), "Top bytes per site" ,output, chart)
