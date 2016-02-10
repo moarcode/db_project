1f52cb12d6b5ad18dfd4f1491d2f991816ab8521
Mateusz Pacek <pacolskb@gmail.com>
2016-02-10 00:01:24 +0000

    wersja finalna

--- init.py
+++ init.py
@@ -12,8 +12,15 @@ def usage():
     print ("""
     -h --help : Pomoc
     -q        : Wykonaj query top_ip, http_stat, http_stat_by_ip
-    -c        : Typ wykresu (pie, bar)
-    """)
+    -c        : Typ wykresu (pie, bar)"""
+    )
+
+def available_queries():
+    print ("""Dostepne zapytania:
+             top-[ip, req, stat]         : Licznik wystapien IP, stron, statusow HTTP
+             byte-req                    : Strony z najwieksza iloscia wyslanych danych
+             byte-perc-[ip, req, stat]   : Procentowy udzial bajow w stosunku do calosci ruchu"""
+             )
 
 # Odczyt argumentow z linii polecen
 
@@ -29,33 +36,18 @@ def cons_dict(q):
     for i in cursor:
         dict[i[0]]=i[1]
 
-    cursor.close()
-    cnx.close()
     return dict
 
-# Obsluga zapytan TOP 
-def top_visited(top_num):
-    query = ("""select var_host, count(var_host) 
-            from apache_log2 group by var_host 
-            order by count(var_host) desc limit %s""" 
-            % top_num
+# Zliczenie wystapien (var_host, var_request, enu_http_status)
+def top_count(top_num, v):
+    query = (""" select %s as f, count(*) as c
+            from apache_log2 group by f 
+            order by c desc limit %s""" 
+            % (v, top_num)
             )
     return cons_dict(query)
 
-def top_stat(top_num):
-    query = ("""select enu_http_status, count(*) as count 
-            from apache_log2 group by enu_http_status 
-            order by count desc limit %s """ % top_num
-            )
-    return cons_dict(query)
-
-def top_stat_by_ip(top_num):
-    query = (""" select var_host, count(*) as count 
-            from apache_log2 group by var_host order by count 
-            desc limit %s """ % top_num
-            )
-    return cons_dict(query)
-
+# Zliczenie bajtow dla requestow z ucieciem adresow dluzych niz 20 znakow
 def top_bytes_per_site(top_num):
     query = (""" select if (CHAR_LENGTH(var_request) > 20, 
             CONCAT(LEFT(var_request, 20), '...'), var_request), 
@@ -64,10 +56,11 @@ def top_bytes_per_site(top_num):
             )
     return cons_dict(query)
 
-def perc_bytes_per_ip(top_num):
-    query = (""" select var_host, sum(int_bytes) / (select sum(int_bytes) 
-                from apache_log2) * 100 as t from apache_log2 group by var_host 
-                order by t desc limit %s""" % top_num
+# Procentowy udzial bajtow zgrupowany po :(var_host, var_request, enu_http_status)
+def perc_bytes_per_var(top_num, v):
+    query = (""" select %s as f, sum(int_bytes) / (select sum(int_bytes) 
+                from apache_log2) * 100 as t from apache_log2 group by f
+                order by t desc limit %s""" % (v, top_num)
                 )
     return cons_dict(query)
 
@@ -110,4 +103,25 @@ if __name__ == "__main__":
         usage()
         sys.exit(2)
 
-    gen_chart(perc_bytes_per_ip(top_num), "Top bytes per site" ,output, chart)
+    #gen_chart(perc_bytes_per_var(top_num, "enu_http_status"), "Top bytes per site" ,output, chart)
+
+    if query == "top-ip":
+        gen_chart(top_count(top_num, "var_host"), "Top IPs conneced to server", output, chart)
+    elif query == "top-req":
+        gen_chart(top_count(top_num, "var_request"), "Top sites requested", output, chart)
+    elif query == "top-stat":
+        gen_chart(top_count(top_num, "enu_http_status"), "Top HTTP responses", output, chart)
+    elif query == "byte-req":
+        gen_chart(top_bytes_per_site(top_num, "var_request"), "Bytes count per site", output, chart)
+    elif query == "byte-perc-ip":
+        gen_chart(perc_bytes_per_var(top_num, "var_host"), "Bytes per IP according to all bytes requested [%]", output, chart)
+    elif query == "byte-perc-req":
+        gen_chart(perc_bytes_per_var(top_num, "var_request"), "Bytes requestes for site / all bytes [%]", output, chart)
+    elif query == "byte-perc-stat":
+        gen_chart(perc_bytes_per_var(top_num, "var_request"), "Bytes requestes in comparation to HTTP responses", output, chart)
+    else:
+        print "Please select proper query"
+        available_queries()
+
+    cursor.close()
+    cnx.close()
